c2d2e85ca91c3bdf4982659994514d62008952f3
Tristan Rasmussen <tristanisthebest@gmail.com>
2016-01-23 22:49:44 -0800

    Better loading and processing.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 A minimal example to train a RNN on characters using theano and lasagne.
 """
 import argparse
+import random
 
 import load_data
 import network
@@ -11,10 +12,19 @@ import network
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('corpus')
+    parser.add_argument('--sample', type=int)
+    parser.add_argument('--load', action='store_true')
     args = parser.parse_args()
-    
+
     data = load_data.load_corpus(args.corpus)
-    model = network.create_and_train_model(data)
+    if args.sample:
+        model = network.load('fit_data')
+        network.preprocess_data(data)
+        seed_start = random.randint(0, len(data) - 20 - 1)
+        seed = data[seed_start:seed_start+20]
+        print(network.sample_model(model, int(args.sample), seed, len(network.CHAR_INDICES) + 1))
+    else: # Train the model
+        model = network.create_and_train_model(data, args.load)
 
 if __name__ == "__main__":
     main()
--- network.py
+++ network.py
@@ -12,17 +12,19 @@ import numpy
 SEQUENCE_LENGTH = 20
 STEP_SIZE = 20
 
-CHAR_INDICES = {}
-INDICIES_CHARS = {}
+CHAR_INDICES = {chr(i): i for i in range(0, 256)}
+INDICIES_CHARS = {i:chr(i) for i in range(0, 256)}
 
 def dump(model, filename):
     json_string = model.to_json()
     open(filename + '.json', 'w').write(json_string)
-    model.save_weights(filename + '.h5')
+    model.save_weights(filename + '.h5', overwrite=True)
 
 def load(filename):
-    model = model_from_json(open(filename + '.json').read())
+    print("Loading model")
+    model = keras.models.model_from_json(open(filename + '.json').read())
     model.load_weights(filename + '.h5')
+    return model
 
 def preprocess_data(data):
     """
@@ -31,9 +33,6 @@ def preprocess_data(data):
 
     global INDICIES_CHARS, CHAR_INDICES
     print("Processing input data")
-    chars = set(data)
-    CHAR_INDICES = dict((c, i) for i, c in enumerate(chars))
-    INDICIES_CHARS = dict((i, c) for i, c in enumerate(chars))
     all_sequences = []
     next_chars = []
     for i in range(0, len(data) - SEQUENCE_LENGTH, STEP_SIZE):
@@ -41,26 +40,26 @@ def preprocess_data(data):
         next_chars.append(data[i+SEQUENCE_LENGTH])
     print("Split into {} sequences".format(len(all_sequences)))
 
-    input_data = numpy.zeros((len(all_sequences), SEQUENCE_LENGTH, len(chars)), dtype=numpy.bool)
-    expected = numpy.zeros((len(all_sequences), len(chars)), dtype=numpy.bool)
+    input_data = numpy.zeros((len(all_sequences), SEQUENCE_LENGTH, 256), dtype=numpy.bool)
+    expected = numpy.zeros((len(all_sequences), 256), dtype=numpy.bool)
     for i, sequence in enumerate(all_sequences):
         for t, char in enumerate(sequence):
             input_data[i, t, CHAR_INDICES[char]] = 1
         expected[i, CHAR_INDICES[next_chars[i]]] = 1
-    return input_data, expected, len(chars)
+    return input_data, expected
 
-def build_model(inputs):
+def build_model():
     """
     Build and compile the model.
     """
 
     print("Building the model.")
     model = keras.models.Sequential()
-    model.add(keras.layers.recurrent.LSTM(512, return_sequences=True, input_shape=(SEQUENCE_LENGTH, inputs)))
+    model.add(keras.layers.recurrent.LSTM(512, return_sequences=True, input_shape=(SEQUENCE_LENGTH, 256)))
     model.add(keras.layers.core.Dropout(0.2))
     model.add(keras.layers.recurrent.LSTM(512, return_sequences=False))
     model.add(keras.layers.core.Dropout(0.2))
-    model.add(keras.layers.core.Dense(inputs))
+    model.add(keras.layers.core.Dense(256))
     model.add(keras.layers.core.Activation('softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
     return model
@@ -71,7 +70,7 @@ def sample(a, temperature=1.0):
     a = numpy.exp(a) / numpy.sum(numpy.exp(a))
     return numpy.argmax(numpy.random.multinomial(1, a, 1))
 
-def sample_model(model, sample_length, seed, inputs):
+def sample_model(model, sample_length, seed):
     """
     Create a sample from the model.
     """
@@ -79,7 +78,7 @@ def sample_model(model, sample_length, seed, inputs):
     result = seed
     sequence = seed
     for _ in range(sample_length):
-        x = numpy.zeros((1, SEQUENCE_LENGTH, inputs))
+        x = numpy.zeros((1, SEQUENCE_LENGTH, 256))
         for t, char in enumerate(sequence):
             x[0, t, CHAR_INDICES[char]] = 1
         preds = model.predict(x, verbose=0)[0]
@@ -90,27 +89,27 @@ def sample_model(model, sample_length, seed, inputs):
     return result
 
 
-def train_model(model, input_data, expected, inputs, print_sample=False, print_seed=None):
+def train_model(model, input_data, expected, print_sample=False, print_seed=None):
     """
     Train a model. Optionally print generated text every epoch.
     """
 
     for epoch in range(50):
-        #model.fit(input_data, expected, batch_size=128, nb_epoch=1)
+        model.fit(input_data, expected, batch_size=128, nb_epoch=1)
         dump(model, 'fit_data')
         print("Finished training epoch {}".format(epoch))
         if print_sample:
-            print(sample_model(model, 100, print_seed, inputs))
+            print(sample_model(model, 100, print_seed))
 
 def create_and_train_model(data, load_from=None):
     """
     Create a model and train it on data.
     """
 
-    input_data, expected, inputs = preprocess_data(data)
+    input_data, expected = preprocess_data(data)
     if load_from:
         model = load('fit_data')
     else:
-        model = build_model(inputs)
+        model = build_model()
     seed_start = random.randint(0, len(data) - SEQUENCE_LENGTH - 1)
-    train_model(model, input_data, expected, inputs, True, data[seed_start:seed_start+SEQUENCE_LENGTH])
+    train_model(model, input_data, expected, True, data[seed_start:seed_start+SEQUENCE_LENGTH])
